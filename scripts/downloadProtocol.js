23f4a7261ef351738fda5cf02fa792a64a91643c
christian-bromann <mail@christian-bromann.com>
2021-06-21 10:33:10 +0200

    upload Sauce protocol extension

--- scripts/downloadProtocol.js
+++ scripts/downloadProtocol.js
@@ -5,17 +5,22 @@ const got = require('got')
 const path = require('path')
 const yaml = require('js-yaml')
 
-const protocolUrl = 'https://raw.githubusercontent.com/zaproxy/zap-api-docs/master/openapi.yaml'
+const protocolUrls = {
+    zap: 'https://raw.githubusercontent.com/zaproxy/zap-api-docs/master/openapi.yaml',
+    sauce: 'https://raw.githubusercontent.com/saucelabs/node-zap/main/protocol/sauce.yaml?token=AAFSRSPP5G42STBK6C2L5ALA3GEHQ'
+}
 
-;(async function run () {
+async function download ([protocolName, protocolUrl]) {
     const apiYaml = await got(protocolUrl)
     const api = await yaml.load(apiYaml.body)
     await fs.promises.writeFile(
-        path.join(__dirname, '..', 'build', 'api', 'zap.json'),
+        path.join(__dirname, '..', 'build', 'api', `${protocolName}.json`),
         JSON.stringify(api, null, 4),
         'utf-8'
     )
-})().then(
+}
+
+Promise.all(Object.entries(protocolUrls).map(download)).then(
     () => console.log('Successfully downloaded ZapProxy API'),
     (err) => console.log(`Error downloading ZapProxy API: ${err.message}`)
 )
